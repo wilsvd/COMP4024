abdf1d12d5bd7ae4b9b64ec54db6ab474833699e
Wil <86851834+wilsvd@users.noreply.github.com>
2024-03-02 20:17:16 +0000

    feat: track player state across game scenes

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Other player-related variables and methods...
+
+    private static Player instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            // If no instance exists, set the instance to this object
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // If an instance already exists, destroy this object
+            Destroy(gameObject);
+        }
+    }
+
+    public void ResetPlayer(GameManager.Level level)
+    {
+        GetComponent<PlayerController>().RepawnPlayer();
+        GetComponent<heath_bar>().InitialiseHealth();
+        if (level != GameManager.Level.Boss) 
+            transform.GetChild(0).GetComponent<Inventory>().ResetInventory();
+    }
+
+}
